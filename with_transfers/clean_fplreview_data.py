05067bcbe40aaaa6b7a0662a930f3b0addbb5e80
Chris Musson <chris.musson@hotmail.com>
2020-09-18 19:08:05 +0100

    Update data preprocessing for use with new optimiser

--- with_transfers/clean_fplreview_data.py
+++ with_transfers/clean_fplreview_data.py
@@ -5,10 +5,10 @@ import sys
 
 df = pd.read_csv(in_file)
 df.rename(columns={x:x.lower() for x in df.columns}, inplace=True)
-df.rename(columns={"bv": "cost"}, inplace=True)
+df.rename(columns={"bv": "buy_cost", "sv": "sale_value"}, inplace=True)
 df["id"] = range(1, len(df) + 1)
 df.dropna(inplace=True)
 cols = [f"{i}_pts" for i in range(int(start_gw), int(end_gw) + 1)]
-df = df.loc[:, ["id", "team", "pos", "name", "cost"] + cols]
+df = df.loc[:, ["id", "team", "pos", "name", "buy_cost", "sale_value"] + cols]
 
 df.to_csv("cleaned_data.csv", index=False)
